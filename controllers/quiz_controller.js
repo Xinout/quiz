39239f67a7feac3e988e8e6901c27cfe8d27aa98
Xinout <s_alcu@hotmail.com>
2015-07-09 20:38:25 +0200

    Modulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,3 +1,5 @@
+var models = require('../models/models.js');
+
 // GET /quizes/question
 exports.question = function(req, res) {
    res.render('quizes/question', {pregunta: 'Capital de Italia'});
@@ -14,4 +16,14 @@ exports.answer = function(req, res) {
 
 exports.author = function(req, res) {
    res.render('quizes/author', {});
+};
+
+// GET /quizes/busqueda
+exports.busqueda = function(req, res) {
+  if (req.query.search != undefined)
+  {
+  models.Quiz.findAll({where: ["pregunta like ?","%"+req.query.search+"%"],order: 'pregunta ASC'}).success(function(quizes) {
+    res.render('quizes/busqueda', { quizes: quizes});
+  })
+}else res.render('quizes/busqueda', { quizes: {}});
 };
--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,6 @@ router.get('/', function(req, res) {
 router.get('/quizes/question',  quizController.question);
 router.get('/quizes/answer',  quizController.answer);
 router.get('/quizes/author',  quizController.author);
+router.get('/quizes/busqueda',  quizController.busqueda);
 
 module.exports = router;
